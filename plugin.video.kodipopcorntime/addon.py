0e93521912d8f9d45d32902698b455624245b00a
tdbaddon <tdbaddon@hotmail.com>
2015-06-22 14:24:25 -0400

    3.50

---
+++ plugin.video.kodipopcorntime/addon.py
@@ -0,0 +1,12 @@
+import sys, os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
+from kodipopcorntime import monkey_patches, plugin
+
+if __name__ == '__main__':
+    try:
+        plugin.run()
+    except Exception, e:
+        import xbmc
+        import traceback
+        map(xbmc.log, traceback.format_exc().split("\n"))
+        raise
