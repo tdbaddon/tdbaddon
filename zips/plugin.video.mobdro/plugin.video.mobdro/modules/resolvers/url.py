5b5181a325639c658a1f1d6094e814fe6972d471
tdbaddon <tdbaddon@hotmail.com>
2015-08-31 01:39:11 -0400

    4.45

--- zips/plugin.video.mobdro/plugin.video.mobdro/modules/resolvers/url.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def parse_url(params):
-    return params
--- zips/plugin.video.mobdro/plugin.video.mobdro/modules/resolvers/youtube.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def parse_youtube(params):
-    return "plugin://plugin.video.youtube/?path=/root/video&action=play_video&videoid=" + params
