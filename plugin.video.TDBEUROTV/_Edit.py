76ab0bc4c897fcc1324e2609071540cba3e89d26
tdbaddon <tdbaddon@hotmail.com>
2017-01-13 12:57:40 -0500

    858

--- plugin.video.TDBEUROTV/_Edit.py
+++ plugin.video.TDBEUROTV/_Edit.py
@@ -1,4 +1,4 @@
 import xbmcaddon
 
 MainBase = 'http://www.kgroup.tcomputers.ca/@TDBEURO/TDBEUROTV-HOME.txt'
-addon = xbmcaddon.Addon('plugin.video.TDBEURO')
+addon = xbmcaddon.Addon('plugin.video.TDBEUROTV')
